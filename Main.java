75db7f98a331dfe9690069247a7a0b291b631c89
Alexandr Kravchenko <sasha121196@mail.ru>
2020-09-21 13:33:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
 
 public class Main {
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
+        if (args.length != 3){
+            System.out.println("You need three arguments: .sam (or bam) .txt(reference) .txt(output) ");
+            System.exit(1);
+        }
         Parser parser = new Parser(args[0], args[1], args[2]);
-        parser.parse();
+        try {
+            parser.parse();
+        } catch (IOException e) {
+            System.out.println("IOException. You need three arguments: .smm (or bmm) .txt(reference) .txt(output) ");
+        }
     }
 }
